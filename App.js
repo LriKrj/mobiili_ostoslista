45bd997dbd1b8277b15253b719717784ed68b749
LriKrj <lauri.karjalainen3@hotmail.com>
2022-09-04 18:43:40 +0300

    valmis

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [purchase, setPurchase] = useState("");
+  const [list, setList] = useState([]);
+
+  const add = () => {
+    setList([...list, {key:purchase}]);
+  };
+  const clear = () => {
+    setList("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(purchase) => setPurchase(purchase)}
+        value={purchase}
+      />
       <StatusBar style="auto" />
+      <View style={styles.operator}>
+        <View style={styles.button}>
+          <Button onPress={add} title="Add" />
+        </View>
+        <View style={styles.button}>
+          <Button onPress={clear} title="Clear" />
+        </View>
+      </View>
+      <Text>Shopping list:</Text>
+      <FlatList style={styles.list} 
+        data={list}
+        renderItem={({ item }) => 
+          <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+       />
     </View>
   );
 }
@@ -13,8 +50,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 150,
+    
   },
+  operator: {
+    flexDirection: "row",
+  },
+  list: {
+    margin:5,
+    },
+  input: {
+    margin: 5,
+    width: 250,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  button: {
+    width: 60,
+  },
+  
+  
 });
